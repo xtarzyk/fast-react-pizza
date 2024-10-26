0d8d293298c487ba8ddf4b11a736207293b88de5
xtarzyk <xtarzyk@tutanota.com>
2024-10-26 19:14:14 +0200

    loader adder

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,12 +1,18 @@
 import Header from "./Header";
-import CartOverview from '../features/cart/CartOverview';
+import CartOverview from "../features/cart/CartOverview";
+import { Outlet, useNavigation } from "react-router-dom";
+import Loader from "./Loader";
 
 function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
+
   return (
-    <div>
+    <div className="layout">
+      {isLoading && <Loader />}
       <Header />
       <main>
-        <h1>Content</h1>
+        <Outlet />
       </main>
       <CartOverview />
     </div>
