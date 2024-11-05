c94711b9bc01d3060b0393421797281531db8465
xtarzyk <xtarzyk@tutanota.com>
2024-11-05 21:01:24 +0100

    Cart, CartItem, MenuItem, CreateOrder, Button styled

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,6 +17,7 @@ module.exports = {
       { allowConstantExport: true },
     ],
     "no-unused-vars": "warn",
+    "react/prop-types": 0,
   },
 };
 // extend = "react-app";
--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,5 +1,3 @@
-/* eslint-disable react-refresh/only-export-components */
-import { useState } from "react";
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 import Button from "../../ui/Button";
